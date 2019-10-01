3c8b772e03c4a2a7e4cabec2928fe47311884d98
Kelechi Aligwo <52804820+Vickel77@users.noreply.github.com>
2019-10-01 09:43:36 +0100

    first commit

---
+++ JS/west.js
@@ -0,0 +1,183 @@
+function home(){
+		var skew_bg1 = document.getElementById('skew_bg1');
+		skew_bg1.style.display='block';
+		var txt1 = document.getElementById('txt1');
+		txt1.style.display='block';
+		var skew_bg2 = document.getElementById('skew_bg2');
+		skew_bg2.style.display='none';
+		var txt2 = document.getElementById('txt2');
+		txt2.style.display='none';
+		var skew_bg3 = document.getElementById('skew_bg3');
+		skew_bg3.style.display='none';
+		var txt3 = document.getElementById('txt3');
+		txt3.style.display='none';
+		var skew_bg4 = document.getElementById('skew_bg4');
+		skew_bg4.style.display='none';
+		var txt4 = document.getElementById('txt4');
+		txt4.style.display='none';
+
+		var full_bg2 = document.getElementById('full_bg2');
+		full_bg2.style.display = 'none';
+		var txt2_full = document.getElementById('txt2_full');
+		txt2_full.style.display = 'none';
+		var full_bg3 = document.getElementById('full_bg3');
+		full_bg3.style.display = 'none';
+		var txt3_full = document.getElementById('txt3_full');
+		txt3_full.style.display = 'none';
+		var full_bg4 = document.getElementById('full_bg4');
+		full_bg4.style.display = 'none';
+		var txt4_full = document.getElementById('txt4_full');
+		txt4_full.style.display = 'none';
+	}
+	function about(){
+		var skew_bg1 = document.getElementById('skew_bg1');
+		skew_bg1.style.display='none';
+		var txt1 = document.getElementById('txt1');
+		txt1.style.display='none';
+		var skew_bg2 = document.getElementById('skew_bg2');
+		skew_bg2.style.display='block';
+		var txt2 = document.getElementById('txt2');
+		txt2.style.display='block';
+		var skew_bg3 = document.getElementById('skew_bg3');
+		skew_bg3.style.display='none';
+		var txt3 = document.getElementById('txt3');
+		txt3.style.display='none';
+		var skew_bg4 = document.getElementById('skew_bg4');
+		skew_bg4.style.display='none';
+		var txt4 = document.getElementById('txt4');
+		txt4.style.display='none';
+
+		var full_bg2 = document.getElementById('full_bg2');
+		full_bg2.style.display = 'none';
+		var txt2_full = document.getElementById('txt2_full');
+		txt2_full.style.display = 'none';
+		var full_bg3 = document.getElementById('full_bg3');
+		full_bg3.style.display = 'none';
+		var txt3_full = document.getElementById('txt3_full');
+		txt3_full.style.display = 'none';
+		var full_bg4 = document.getElementById('full_bg4');
+		full_bg4.style.display = 'none';
+		var txt4_full = document.getElementById('txt4_full');
+		txt4_full.style.display = 'none';
+
+	}
+	function portfolio(){
+		var skew_bg1 = document.getElementById('skew_bg1');
+		skew_bg1.style.display='none';
+		var txt1 = document.getElementById('txt1');
+		txt1.style.display='none';
+		var skew_bg2 = document.getElementById('skew_bg2');
+		skew_bg2.style.display='none';
+		var txt2 = document.getElementById('txt2');
+		txt2.style.display='none';
+		var skew_bg3 = document.getElementById('skew_bg3');
+		skew_bg3.style.display='block';
+		var txt3 = document.getElementById('txt3');
+		txt3.style.display='block';
+		var skew_bg4 = document.getElementById('skew_bg4');
+		skew_bg4.style.display='none';
+		var txt4 = document.getElementById('txt4');
+		txt4.style.display='none';
+
+		var full_bg2 = document.getElementById('full_bg2');
+		full_bg2.style.display = 'none';
+		var txt2_full = document.getElementById('txt2_full');
+		txt2_full.style.display = 'none';
+		var full_bg3 = document.getElementById('full_bg3');
+		full_bg3.style.display = 'none';
+		var txt3_full = document.getElementById('txt3_full');
+		txt3_full.style.display = 'none';
+		var full_bg4 = document.getElementById('full_bg4');
+		full_bg4.style.display = 'none';
+		var txt4_full = document.getElementById('txt4_full');
+		txt4_full.style.display = 'none';
+	}
+	function team(){
+		var skew_bg1 = document.getElementById('skew_bg1');
+		skew_bg1.style.display='none';
+		var txt1 = document.getElementById('txt1');
+		txt1.style.display='none';
+		var skew_bg2 = document.getElementById('skew_bg2');
+		skew_bg2.style.display='none';
+		var txt2 = document.getElementById('txt2');
+		txt2.style.display='none';
+		var skew_bg3 = document.getElementById('skew_bg3');
+		skew_bg3.style.display='none';
+		var txt3 = document.getElementById('txt3');
+		txt3.style.display='none';
+		var skew_bg4 = document.getElementById('skew_bg4');
+		skew_bg4.style.display='block';
+		var txt4 = document.getElementById('txt4');
+		txt4.style.display='block';
+
+		var full_bg2 = document.getElementById('full_bg2');
+		full_bg2.style.display = 'none';
+		var txt2_full = document.getElementById('txt2_full');
+		txt2_full.style.display = 'none';
+		var full_bg3 = document.getElementById('full_bg3');
+		full_bg3.style.display = 'none';
+		var txt3_full = document.getElementById('txt3_full');
+		txt3_full.style.display = 'none';
+		var full_bg4 = document.getElementById('full_bg4');
+		full_bg4.style.display = 'none';
+		var txt4_full = document.getElementById('txt4_full');
+		txt4_full.style.display = 'none';
+	}
+
+	function show_more_about(){
+		var skew_bg2 = document.getElementById('skew_bg2');
+		skew_bg2.style.display = 'none';
+		var txt2 = document.getElementById('txt2');
+		txt2.style.display='none';
+		var full_bg2 = document.getElementById('full_bg2');
+		full_bg2.style.display = 'block';
+		var txt2_full = document.getElementById('txt2_full');
+		txt2_full.style.display = 'block';
+		var full_bg3 = document.getElementById('full_bg3');
+		full_bg3.style.display = 'none';
+		var txt3_full = document.getElementById('txt3_full');
+		txt3_full.style.display = 'none';
+		var full_bg4 = document.getElementById('full_bg4');
+		full_bg4.style.display = 'none';
+		var txt4_full = document.getElementById('txt4_full');
+		txt4_full.style.display = 'none';
+	}
+
+	function show_more_portfolio(){
+		var full_bg3 = document.getElementById('full_bg3');
+		full_bg3.style.display = 'block';
+		var txt3_full = document.getElementById('txt3_full');
+		txt3_full.style.display = 'block';
+		var skew_bg3 = document.getElementById('skew_bg2');
+		skew_bg3.style.display = 'none';
+		var txt3 = document.getElementById('txt2');
+		txt3.style.display='none';
+		var full_bg2 = document.getElementById('full_bg2');
+		full_bg2.style.display = 'none';
+		var txt2_full = document.getElementById('txt2_full');
+		txt2_full.style.display = 'none';
+		var full_bg4 = document.getElementById('full_bg4');
+		full_bg4.style.display = 'none';
+		var txt4_full = document.getElementById('txt4_full');
+		txt4_full.style.display = 'none';
+	}
+
+	function show_more_team(){
+		var full_bg4 = document.getElementById('full_bg4');
+		full_bg4.style.display = 'block';
+		var txt4_full = document.getElementById('txt4_full');
+		txt4_full.style.display = 'block';
+		var skew_bg4 = document.getElementById('skew_bg4');
+		skew_bg4.style.display='none';
+		var txt4 = document.getElementById('txt4');
+		txt4.style.display='none';
+		var full_bg2 = document.getElementById('full_bg2');
+		full_bg2.style.display = 'none';
+		var txt2_full = document.getElementById('txt2_full');
+		txt2_full.style.display = 'none';
+		var full_bg3 = document.getElementById('full_bg3');
+		full_bg3.style.display = 'none';
+		var txt3_full = document.getElementById('txt3_full');
+		txt3_full.style.display = 'none';
+		
+	}
